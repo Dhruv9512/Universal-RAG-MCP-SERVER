2a0141abffe3a1ef6cfdba1138face55b9d3a83f
Dhruv9512 <dhruvsharma56780@gmail.com>
2026-01-26 21:01:04 +0530

    remove mcp.run() for deployment

--- main.py
+++ main.py
@@ -174,10 +174,10 @@ def rag_retrieve_and_rerank(
 # --------------------------------------------------
 # RUN SERVER
 # --------------------------------------------------
-if __name__ == "__main__":
-    mcp.run(
-        transport="sse",
-        host="0.0.0.0",
-        port=8000,
-    )
+# if __name__ == "__main__":
+#     mcp.run(
+#         transport="sse",
+#         host="0.0.0.0",
+#         port=8000,
+#     )
     # mcp.run()
